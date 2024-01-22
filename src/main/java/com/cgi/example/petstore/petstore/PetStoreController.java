62be7a1e956d10ad33b1c22aec7b7753556fe24c
Paul Armstrong <paul.armstrong@cgi.com>
2024-01-22 16:21:42 +0000

    Type and status added. Endpoint created. Works with static data.

--- src/main/java/com/cgi/example/petstore/petstore/PetStoreController.java
+++ src/main/java/com/cgi/example/petstore/petstore/PetStoreController.java
@@ -2,16 +2,32 @@ package com.cgi.example.petstore.petstore;
 
 import com.cgi.example.petstore.api.PetStoreApi;
 import com.cgi.example.petstore.model.Pet;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class PetStoreController implements PetStoreApi {
+
+
+    @Override
+    public ResponseEntity<List<Pet>> findPetsByStatus(@RequestParam(name="status") String status) {
+        // Creating a dummy list of pets
+        List<Pet> allPets = List.of(
+                createDummyPet(1L, "Kitty", "CAT", "SOLD"),
+                createDummyPet(2L, "Doggie", "DOG", "PENDING_COLLECTION"),
+                createDummyPet(3L, "Piggie", "GUINEA_PIG", "AVAILABLE_FOR_PURCHASE")
+        );
+
+        List<Pet> filteredPets = allPets.stream()
+                .filter(pet -> pet.getPetStatus().equals(Pet.PetStatusEnum.valueOf(status)))
+                .toList();
+
+        return ResponseEntity.ok(filteredPets);
+    }
 
     public ResponseEntity<Pet> getPetById(Long petId) {
 
@@ -20,5 +36,17 @@ public class PetStoreController implements PetStoreApi {
         newPet.setName("Static Name");
 
         return ResponseEntity.ok(newPet);
+    }
+
+
+
+    private Pet createDummyPet(Long id, String name, String petType, String petStatus) {
+        Pet pet = new Pet();
+        pet.setId(id);
+        pet.setName(name);
+        pet.setPetType(Pet.PetTypeEnum.valueOf(petType));
+        pet.setPetStatus(Pet.PetStatusEnum.valueOf(petStatus));
+
+        return pet;
     }
 }
